46d7438a8911ef87b3920ba6253e8883e30ee596
vaibhav garg <vaibhav.garg02@motherson.com>
2025-07-22 19:15:36 +0530

    policy false change

--- src/app/Forms/templateform/templateform.component.ts
+++ src/app/Forms/templateform/templateform.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-templateform',
   templateUrl: './templateform.component.html',
-  styleUrl: './templateform.component.scss'
+  styleUrl: './templateform.component.scss',
 })
-export class TemplateformComponent {
-
-}
+export class TemplateformComponent {}
